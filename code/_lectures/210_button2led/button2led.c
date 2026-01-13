8b23fc73adad65850c66ef4c89a15aac5afb27a3
kongkrit <45794368+kongkrit@users.noreply.github.com>
2026-01-14 01:20:30 +0700

    Update button2led.c

--- code/_lectures/210_button2led/button2led.c
+++ code/_lectures/210_button2led/button2led.c
@@ -1,6 +1,30 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include "baremetal_binary.h"
+
+// MODEL and initializer
+uint8_t state = B8(00000000);
+// CONTROLLER
+#define PUSHBUTTON_BASE ((volatile uint8_t *)0xD000U)
+// VIEW
+#define LED_STRIP ((volatile uint8_t *)0xE010U)
 
 void main(void){
     
+    uint8_t bit_mask;
+
+    while (true) {
+        // CONTROLLER, set bit mask
+        bit_mask = B8(00000000);
+        for (uint8_t i = 0; i < 8; i = i + 1) {
+            if (*(PUSHBUTTON_BASE + i)) {
+                bit_mask = 1U << (7-i);
+                break;
+            }
+        }
+        // UPDATE MODEL
+        state = state ^ bit_mask;
+        // VIEW
+        *LED_STRIP = state;
+    }
 }
