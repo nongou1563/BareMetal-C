62e5417823a42904794aedc9bd16837635ae24b5
Thampapon <nongou1563@gmail.com>
2026-02-01 20:12:10 +0700

    Update hardware_04.c

--- code/homeworks/homework02-problem4/hardware_04.c
+++ code/homeworks/homework02-problem4/hardware_04.c
@@ -1,16 +1,35 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define LED0 ((volatile bool *)0xE000U)
+#define LED1 ((volatile bool *)0xE001U)
+#define LED2 ((volatile bool *)0xE002U)
 
 void main(void) {
 
-    volatile bool *led; 
-    led = LED0;
+    volatile bool *led01; 
+    led01 = LED1;
+    volatile bool *led02; 
+    led02 = LED2;
+    
 
     while (true) {
-        *led = true;
-        *led = false;
+        *led01 = true;
+        *led01 = false;
+        
+        *led02 = true;
+        *led02 = false;
+
+        *led01 = true;
+        *led01 = false;
+
+        *led02 = true;
+        *led02 = false;
+
+        *led01 = true;
+        *led01 = false;
+
+        *led02 = true;
+        *led02 = false;
     }
 
 }
