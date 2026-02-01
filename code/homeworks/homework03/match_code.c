a527d015ef325e94b8913fef9076ea0cd044f5db
Thampapon <nongou1563@gmail.com>
2026-02-01 21:27:42 +0700

    Update match_code.c

--- code/homeworks/homework03/match_code.c
+++ code/homeworks/homework03/match_code.c
@@ -1,6 +1,26 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-void main(void) {
+#define LED_ADDR      ((uint8_t *)0xE000)
+#define SWITCH_START  ((uint8_t *)0xC010)
 
+void main(void) {
+    uint8_t target_code[8] = {1, 0, 0, 0, 0, 0, 0, 1};
+
+    while (true) {
+        bool is_match = true;
+
+        for (int i = 0; i < 8; i++) {
+            if (*(SWITCH_START + i) != target_code[i]) {
+                is_match = false;
+                break;
+            }
+        }
+
+        if (is_match) {
+            *LED_ADDR = 1;
+        } else {
+            *LED_ADDR = 0;
+        }
+    }
 }
